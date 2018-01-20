291021e129769a3bed6c113130db598ce50fff5a
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2018-01-20 13:27:49 -0800

    config for heroku

--- server/server.js
+++ server/server.js
@@ -6,8 +6,9 @@ const {mongoose} = require('./db/mongoose')
 
 const {Todo} = require('./models/todo')
 const {User} = require('./models/user')
+const app = express()
 
-let app = express()
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.post('/todos', (req, res)=>{
@@ -42,8 +43,8 @@ app.get('/todos/:id',(req, res)=>{
         res.status(400).send();
     })
 })
-app.listen(3000, ()=>{
-    console.log('Listening on port 3000')
+app.listen(port, ()=>{
+    console.log(`started up at ${port}`)
 })
 
 module.exports = {app};
