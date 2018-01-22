13c89a07e232e634a22f97cbc99b6b33d4eab6b2
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2018-01-22 12:06:30 -0800

    added jwt, user schema, token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
-
-let User = mongoose.model('User',{
+const jwt = require('jsonwebtoken')
+const _ = require('lodash')
+let UserSchema = new mongoose.Schema({
     email:{
         type: String,
         minlength: 5,
@@ -28,6 +29,27 @@ let User = mongoose.model('User',{
             required: true
         }
     }]
-});
+})
+
+// Method override on how method handles certain things
+
+UserSchema.methods.toJSON = function(){
+    let user = this
+    let userObject = user.toObject()
+
+    return _.pick(userObject, ['_id', 'email'])
+}
+UserSchema.methods.generateAuthToken = function (){
+    let user = this;
+    let access = 'auth';
+    let token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens.push({access, token})
+
+    return user.save().then(()=>{
+        return token;
+    });
+};
+let User = mongoose.model('User', UserSchema);
 
 module.exports = {User}
